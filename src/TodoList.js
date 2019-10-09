c25f9f874d308054f6b69e7b97b5f0d01bc61dfd
changuitoss <ezealvarezrubio@gmail.com>
2019-10-09 09:14:39 -0300

    Creando el componente addItem

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			items: []
+		};
+
+		this.addItem = this.addItem.bind(this);
+	}
+
+	addItem(e) {
+
+	}
+
+
 	render() {
 		return (
 				<div className=''>
 					<div className=''>
-						<form>
+						<form onSubmit={this.addItem}>
 							<input type='text' placeholder='add items'></input>
 							<button type='submit'>Add</button>
 						</form>
